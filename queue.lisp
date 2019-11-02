d190db1fd29efcda95547a4d8d868d7e8a6e5be8
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-02 06:10:20 -0400

    fix q-push

--- queue.lisp
+++ queue.lisp
@@ -5,8 +5,8 @@
 (defclass queue ()
   ((q :accessor q :initform nil)))
 
-(defmethod q-push ((q queue) (m e/message:message))
-  (setf q (append q (list m))))
+(defmethod q-push ((self queue) (m e/message:message))
+  (setf (q self) (append q (list m))))
 
 (defmethod q-pop ((q queue))
   (pop q))
