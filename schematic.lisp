bf7a3ab5fc358ceaa9532e0b7eb62f58a708fc34
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-03 09:41:49 -0500

    wip: removed recursive release-outputs (but havent installed loop to check for outputs yet)

--- schematic.lisp
+++ schematic.lisp
@@ -69,19 +69,15 @@
       (assert success)
       wire)))
 
-(defmethod push-input ((self schematic) (child (eql nil)) (msg e/message:message))
+(defmethod push-input ((self e/schematic:schematic) (child (eql nil)) (msg e/message:message))
   ;; SENDing to an output of the schematic
   ;; return part that has new outputs, else return nil - in this case always (list self)
   ;; push-input always returns a list (or NIL, which is a list)
   (declare (ignore child))
   (e/part:ensure-message-contains-valid-output-pin self msg)
-  (e/part:push-output self msg)
-  (list self))
+  (e/part:push-output self msg))
 
-(defmethod push-input ((self schematic) (child e/part:part) (msg e/message:message))
+(defmethod push-input ((self e/schematic:schematic) (child e/part:part) (msg e/message:message))
   ;; "normal" sending to a child part
-  ;; push-input always returns a list (or NIL, which is a list)
   (declare (ignore self))
-  ;; return part that has new outputs, else return nil - in this case always nil
-  (e/part:push-input child msg)
-  nil)
+  (e/part:push-input child msg))
--- wire.lisp
+++ wire.lisp
@@ -3,23 +3,11 @@
 (defmethod make-wire (&key (receivers cl:CONS))
   (make-instance 'wire :receivers receivers))
 
-(defmethod deliver-message ((schem e/schematic:schematic) (wire (eql nil)) (message e/message:message))
-  ;; return list of parts that have new outputs
-  nil)
-
 (defmethod deliver-message ((schem e/schematic:schematic) (wire e/wire:wire) (message e/message:message))
   ;; we don't copy messages/data - receiver must copy the message-data if it intends to mutate it
-  ;; return list of parts that have newly created outputs
-  (let ((newly-created-outputs-list nil))
-    (mapc #'(lambda (part-pin)
+  (mapc #'(lambda (part-pin)
               (let ((destination-part (e/part-pin:part part-pin))
                     (destination-pin  (e/part-pin:pin  part-pin))
                     (data (e/message:data message)))
-                (append
-                 (e/schematic:push-input schem destination-part (e/message:make-message destination-pin data))
-                 newly-created-outputs-list)))
-          (receivers wire))
-    ;; In most cases, there are no newly created outputs.
-    ;; In the other cases where we have delivered messages to the outputs of a schematic, we are creating new
-    ;; outputs
-     newly-created-outputs-list))
+                (e/schematic:push-input schem destination-part (e/message:make-message destination-pin data))))
+        (receivers wire)))
