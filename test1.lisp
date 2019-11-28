4fafbc118633bed039dc89368017a267e3faccc3
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-28 09:54:59 -0500

    dumbed down test1 to exactly one code part and one schematic part

--- test1.lisp
+++ test1.lisp
@@ -5,31 +5,23 @@
 
   (let ((schem (@new-schematic :name "schem" :output-pins '(:schem-out)))
         (producer (@new-code :name "producer" :output-pins '(:producer-out)))
-        (consumer (@new-code :name "consumer" :input-pins '(:in) :output-pins '(:consumer-out)))
-        (wire1 (@new-wire :name "wire1"))
-        (wire2 (@new-wire :name "wire2")))
+        (wire99 (@new-wire :name "wire99")))
 
     (@top-level-schematic schem)
 
     (@add-part-to-schematic schem producer)
-    (@add-part-to-schematic schem consumer)
 
     (@set-first-time-handler producer #'test1-produce-first-time)
-    (@set-input-handler consumer #'test1-consume-and-forward)
 
-    (@add-receiver-to-wire wire1 (e/part::get-input-pin consumer :in))
-    (@add-receiver-to-wire wire2 (e/part::get-output-pin schem :schem-out))
+    (@add-receiver-to-wire wire99 (e/part::get-output-pin schem :schem-out))
     (let ((pin (e/part::get-output-pin producer :producer-out)))
-      (@add-source-to-schematic schem pin  wire1))
-    (let ((pin (e/part::get-output-pin consumer :consumer-out)))
-      (@add-source-to-schematic schem pin wire2))
+      (@add-source-to-schematic schem pin wire99))
+
+    (setf cl-event-passing-user::*test1-net* schem)  ;; save net for reuse in test-reuse1.lisp
 
     (@start-dispatcher)))
 
 (defmethod test1-produce-first-time ((self e/part:part))
   (@send self (e/part::get-output-pin self :producer-out) "hello from test 1"))
-
-(defmethod test1-consume-and-forward ((self e/part:part) (e e/event:event))
-  (@send self (e/part::get-output-pin self :consumer-out) (e/event:data e)))
 
   
