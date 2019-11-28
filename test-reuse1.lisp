eedd9bcda19f1316c554e99f2ef7cdefaf5a6a98
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-28 10:03:44 -0500

    fixed typo in packaging

---
+++ test-reuse1.lisp
@@ -0,0 +1,24 @@
+(in-package :cl-event-passing-user)
+
+;; reuse existing part test1 inside of a new schematic top-schem
+
+(defun test-reuse1 ()
+  (@initialize)
+
+  (let ((top-schem (@new-schematic :name "top-schem" :output-pins '(:top-schem-out)))
+        (child (@reuse-part cl-event-passing-user::*test1-net*
+                            :name "reused test1" :output-pins '(:schem-out)))
+        (wire2 (@new-wire :name "wire200")))
+
+    (@top-level-schematic schem)
+
+    (@add-part-to-schematic schem child)
+
+    (@add-receiver-to-wire wire200 (e/part::get-output-pin top-schem :top-schem-out))
+
+    (@start-dispatcher)))
+
+  
+(defun cl-user::rtest()
+  ;; convenience function - to be deleted
+  (cl-event-passing-user::test-reuse1))
