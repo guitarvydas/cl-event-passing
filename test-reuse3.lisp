a0342e0d1e15fc21d08a34d89c7c1fdb57c9ac81
Paul Tarvydas <guitarvydas@gmail.com>
2019-11-29 08:49:07 -0500

    add new test test-reuse3

---
+++ test-reuse3.lisp
@@ -0,0 +1,19 @@
+(in-package :cl-event-passing-user)
+
+(defun test-reuse3 ()
+  (@initialize)
+
+  (let ((net 
+         (cl-event-passing-user::@defnetwork cl-event-passing-user::main
+          (:part cl-event-passing-user::*test1-net* child nil (:schem-out))
+          (:schem cl-event-passing-user::main (:main-in) (:main-out)
+                 (child)
+                 ((((child :schem-out)) ((:self :main-out))))))))
+
+    (@start-dispatcher)))
+  
+(defun cl-user::rtest()
+  ;; convenience function - to be deleted
+  (cl-event-passing-user::test1)
+  (cl-event-passing-user::test-reuse3))
+
