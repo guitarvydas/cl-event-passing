1599a40dc2ab736bd2276e68ed517ada0648b5ec
Paul Tarvydas <guitarvydas@gmail.com>
2020-01-22 11:14:46 -0500

    exclude - as first char in <ident>

--- net-parser.lisp
+++ net-parser.lisp
@@ -5,7 +5,7 @@
 (esrap:defrule <dot> (and "." (* <ws>)) (:constant :dot))
 (esrap:defrule <comma> (and "," (* <ws>)))
 ;; exclude . and -
-(esrap:defrule <lisp-id-char-first> (esrap:character-ranges (#\a #\z) (#\A #\Z) #\@ #\* #\~ #\! #\$ #\% #\^ #\- #\+ #\_ #\= #\{ #\} #\[ #\] #\\ #\/ #\< #\> #\?))
+(esrap:defrule <lisp-id-char-first> (esrap:character-ranges (#\a #\z) (#\A #\Z) #\@ #\* #\~ #\! #\$ #\% #\^ #\+ #\_ #\= #\{ #\} #\[ #\] #\\ #\/ #\< #\> #\?))
 ;; exclude .
 (esrap:defrule <lisp-id-char-follow> (or (esrap:character-ranges (#\0 #\9)) #\- <lisp-id-char-first>))
 (esrap:defrule <ident> (and <lisp-id-char-first> (* <lisp-id-char-follow>)) (:text t))
