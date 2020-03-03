bfb3b2b9ac6753a17bab8df10cb0a8a26fcb7d77
Paul Tarvydas <guitarvydas@gmail.com>
2020-03-03 08:09:31 -0500

    ignore junk.*

--- junk.lisp
+++
@@ -1,4 +0,0 @@
-(esrap::clear-rules)
-(esrap:defrule <lisp-id-char-first> (esrap:character-ranges (#\a #\z) (#\A #\Z) #\@ #\* #\~ #\! #\- #\$ #\% #\^ #\- #\+ #\_ #\= #\{ #\} #\[ #\] #\\ #\. #\/ #\< #\> #\?))
-(esrap:defrule <lisp-id-char-follow> (or (esrap:character-ranges (#\0 #\9)) <lisp-id-char-first>))
-(esrap:defrule <ident> (and <lisp-id-char-first> (* <lisp-id-char-follow>)) (:text t))
